8476fcd7b2b85350e8e3293f70fc420d7f5f68ac
jaisonschmidt@gmail.com <Nsx5229!>
2019-08-20 23:52:44 -0300

    Teste de utilização de fake api

--- src/services/UserService/UserService.jsx
+++ src/services/UserService/UserService.jsx
@@ -3,17 +3,17 @@ import Config from '../../config';
 
 class UserService {
     async getAllUsers() {
-        const url = `${Config.URL_API}/users/`;
+        const url = `${Config.URL_API}/user/`;
         return Axios.get(url).then( response => response.data )
     }
 
     async getUser(id) {
-        const url = `${Config.URL_API}/users/${id}`;
+        const url = `${Config.URL_API}/user/${id}`;
         return Axios.get(url).then( response => response.data );
     }
 
     async postNewUser(user) {
-        const url = `${Config.URL_API}/users/`;
+        const url = `${Config.URL_API}/user/`;
         return Axios.post(url, user).then( response => response.data );
     }
 }
